ebe35812050cc1d9327da8f70414187657bf60f0
Ddjo <jocelyn.cordier@gmail.com>
2024-11-27 13:30:02 +0100

    remove shared store

--- libs/shared-store/src/lib/api-token.ts
+++
@@ -1,6 +0,0 @@
-import { InjectionToken } from "@angular/core";
-
-/**
- * necessary for generic effects, calling different API endpoints for each app
- */
-export const API_BOOK_TOKEN = new InjectionToken('apiToken');
